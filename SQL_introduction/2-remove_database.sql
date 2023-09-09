744617340541eaba60121495ee58dde8a40de945
youssra <youssraanene20@gmail.com>
2023-09-09 09:18:56 -0700

    Delete a database

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,8 @@
+-- Attempt to drop the database hbtn_0c_0
+-- If it doesn't exist, no error will occur
+
+-- Try to drop the database
+DROP DATABASE IF EXISTS hbtn_0c_0;
+
+-- This script does not use SELECT or SHOW statements.
+
