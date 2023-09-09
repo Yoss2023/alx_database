1c94c5b7ef92bac61f670cedbd7749a2d48e2e6a
youssra <youssraanene20@gmail.com>
2023-09-09 09:17:45 -0700

    Create a database

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,8 @@
+-- Check if the database hbtn_0c_0 exists
+-- If it doesn't exist, create it
+
+-- Try to create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- This script does not use SELECT or SHOW statements.
+
