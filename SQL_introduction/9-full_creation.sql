f864081ae039ba954812ea4dd13e842448181842
youssra <youssraanene20@gmail.com>
2023-09-09 09:31:29 -0700

    Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- Create the table second_table if it doesn't exist
+-- Add multiple rows to the table
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+-- Add rows to the table
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
+
