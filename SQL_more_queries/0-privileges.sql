716abd4c542c2f5dd5cc5ed5366818a9026a6f78
youssra <youssraanene20@gmail.com>
2023-09-16 08:29:24 -0700

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,6 +2,13 @@
 
 -- You can add comments to describe your SQL statements
 SELECT * FROM users; -- This selects all records from the 'users' table
+-- Check if 'user_0d_1' exists
+SELECT user FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost';
+
+-- Check if 'user_0d_1' has the expected grants
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Attempt to execute a query without selecting a database
+SELECT * FROM non_existing_table;
 
 # Log in to MySQL as the root user
 mysql -h localhost -u root -p
