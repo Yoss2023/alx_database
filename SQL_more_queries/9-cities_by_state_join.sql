bc1a4a4ba253e0fb91da07fa361c87c692079333
youssra <youssraanene20@gmail.com>
2023-09-16 08:43:31 -0700

    Cities by States

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- Use the 'hbtn_0d_usa' database
+USE hbtn_0d_usa;
+
+-- Select cities with state names and sort by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
+
