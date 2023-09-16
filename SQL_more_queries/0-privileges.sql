e9face6d356581568410e9375692ab3856601755
youssra <youssraanene20@gmail.com>
2023-09-16 08:26:25 -0700

    My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,18 @@
+# Log in to MySQL as the root user
+mysql -h localhost -u root -p
+
+# Enter your MySQL root password when prompted
+
+# Create 'user_0d_1' if it doesn't exist and grant privileges
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+
+# Create 'user_0d_2' if it doesn't exist and grant privileges
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
+
+# Exit MySQL
+exit
+
